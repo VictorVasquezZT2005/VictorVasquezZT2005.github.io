e8ef91dac4e6a4d720b79158ff1bb3ca961aed1c
EliseoZT2005 <vvasquezdv2016@icloud.com>
2023-12-28 19:34:33 -0600

    up046

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var typed = new Typed('#element', {
     strings: [
     '<i class="dev">Apps Android</i>',
     '<i class="dev">Custom Roms</i>',
+    '<i class="dev">Recovery Path</i>',
     '<i class="dev">Paginas Web</i>'
   ],
     //stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
@@ -19,11 +20,19 @@ var typed = new Typed('#element', {
   });
 
 const body = document.body;
+const colorButton = document.getElementById('colorButton');
+const icon = document.getElementById('icon');
 
 function toggleMode() {
     // Alternamos entre los modos claro y oscuro en el cuerpo
     body.classList.toggle('light-mode');
     body.classList.toggle('dark-mode');
+
+    // Alternamos entre los modos claro y oscuro para el color del botón
+    changeButtonColor();
+
+    // Cambiamos el color del icono según el modo actual
+    changeIconColor();
 
     // Guardamos la preferencia del usuario en localStorage
     const currentMode = body.classList.contains('dark-mode') ? 'dark' : 'light';
@@ -35,14 +44,28 @@ const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-s
 
 // Configuramos el modo predeterminado según la preferencia del sistema
 if (prefersDarkMode) {
-    body.classList.add('dark-mode');
+      body.classList.add('dark-mode');
 } else {
-    body.classList.add('light-mode');
+      body.classList.add('light-mode');
 }
 
 // Si el usuario ya ha interactuado y seleccionado un modo, aplicamos esa preferencia
 const userPreferredMode = localStorage.getItem('preferred-mode');
 if (userPreferredMode) {
-    body.classList.remove('light-mode', 'dark-mode');
-    body.classList.add(userPreferredMode + '-mode');
+      body.classList.remove('light-mode', 'dark-mode');
+      body.classList.add(userPreferredMode + '-mode');
+}
+
+// Aplicamos el color del botón según el modo actual
+changeButtonColor();
+
+// Aplicamos el color del icono según el modo actual
+changeIconColor();
+
+function changeButtonColor() {
+      colorButton.style.color = body.classList.contains('dark-mode') ? '#fff' : '#000';
+}
+
+function changeIconColor() {
+      icon.style.color = body.classList.contains('dark-mode') ? '#C5EDF5' : '#001C63';
 }
